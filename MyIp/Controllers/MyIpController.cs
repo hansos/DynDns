c5b82733043bd59acf83eb3bf2fc094f8c526948
Hans Olav Sorteberg <hans.olav@sorteberg.com>
2021-08-11 08:48:18 +0200

    Project MyIp added.

---
+++ MyIp/Controllers/MyIpController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Net;
+
+namespace MyIp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MyIpController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return GetUserIPAddress();
+        }
+
+
+        private string GetUserIPAddress()
+        {
+
+            string result;
+            if (Request.Headers.TryGetValue("X-Forwarded-For", out var forwardedIps))
+            {
+                result = forwardedIps.First();
+            }
+            else
+            {
+                result = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+            return result;
+        }
+
+    }
+}
