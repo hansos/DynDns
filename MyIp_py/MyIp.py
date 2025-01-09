1311dec80c312d9a65a028947efbf0344e8754b7
Hans Olav Sorteberg <hans.olav@sorteberg.com>
2025-01-09 08:54:28 +0100

    Add MyIp_py project and update DynDns.sln for VS 2022
    Updated DynDns.sln to Visual Studio 2022 (v17) and added the MyIp_py project. Created MyIp.py with a lambda_handler function to extract source IP from AWS Lambda events. Added MyIp_py.pyproj to define project properties and configurations.

---
+++ MyIp_py/MyIp.py
@@ -0,0 +1,10 @@
+
+import json
+
+def lambda_handler(event, context):
+    ip = event['requestContext']['identity']['sourceIp']
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(ip)
+    }
+    return response
